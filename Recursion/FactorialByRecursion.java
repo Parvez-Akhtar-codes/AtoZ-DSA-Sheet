3bc7e6b74eb9b9cff23a2037fe7d63c41a04496d
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2024-09-17 18:02:31 +0530

    Factorial number

---
+++ Recursion/FactorialByRecursion.java
@@ -0,0 +1,13 @@
+class FactorialByRecursion {
+public static void main(String[] args) {
+    System.out.println(Factorial(5));
+}
+    
+
+static int Factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*Factorial(n-1);
+}
+}
