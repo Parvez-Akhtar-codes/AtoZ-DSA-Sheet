6f83e102b93af5dc4f879ba8d5856f651f143722
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2025-07-07 23:19:51 +0530

    Array

---
+++ Array Questions/LargestElementInArray.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+ class LargestElementInArray {
+    public static int largestElement(int[] nums){
+        int largest = nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>largest){
+                largest=nums[i];
+            }
+        }
+        return largest;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[6];
+        System.out.println("Vaues for array : ");
+        for (int i = 0; i < nums.length; i++) {
+            
+            nums[i] = sc.nextInt();
+        }
+        // System.out.println("the given array is : ");
+        //  for (int i = 0; i < nums.length; i++) {
+            
+        //     System.out.println(nums[i]);
+        // }
+
+
+
+
+        int largestElement = largestElement(nums);
+        System.out.println("Largest element in this array is : ");
+        System.out.println(largestElement);
+        
+        
+    }
+}
